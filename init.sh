c8142d78b74bf6ce0f507c61b15cf192cd100c33
admin <admin@cathy.local>
2019-05-17 19:13:08 +0800

    创建admin用户需要时间，否则会出现创建未成功，后续命令失败

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 (sleep 5 ; \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD ; \
+sleep 2; \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
+sleep 1; \
 rabbitmqctl add_vhost $V_HOST; \
+sleep 1; \
 rabbitmqctl set_permissions -p $V_HOST $RABBITMQ_USER ".*" ".*" ".*" ; \
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***") &
 rabbitmq-server $@
